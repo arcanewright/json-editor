e06a4926000ebcd868569c5e08c492b7ff81db56
ArcaneWright <77860571+arcanewright@users.noreply.github.com>
2021-06-24 12:45:26 -0400

    editing of components implemented

--- src/ContentArea.js
+++ src/ContentArea.js
@@ -11,6 +11,30 @@ function ContentArea (props) {
         }
     }, [props.loaded, props.myData, setCurrent])
 
+
+    const changeElement =  (id, newValue, delElement = false) => {
+
+        console.log(id + newValue)
+        let myPos = current.findIndex((e) => e.id === id)
+        let myElement = current[myPos]
+        console.log(myPos)
+        console.log(myElement)
+        myElement.value = newValue
+
+        let firstHalf = current.slice(0, myPos)
+        let secondHalf = current.slice(myPos + 1)
+        if (delElement) {
+            if (myElement.label) {
+                // Figure this out
+            }
+            setCurrent(firstHalf.concat(secondHalf))
+        }
+        else {
+            setCurrent(firstHalf.concat([myElement], secondHalf))
+        }
+
+    }
+
     const insertElement = (type, elBefore, first) => {
 
         let resultArray = []
@@ -32,7 +56,7 @@ function ContentArea (props) {
             resultArray.push(label)
             resultingObj = {type:type, parent:parent, id:myuuid, value:"", label:labeluuid}
         }
-        else if (current.find((e)=> e.id === parent) === "object") {
+        else if (current.find((e)=> e.id === parent).type === "object") {
             let labeluuid = uuidv4()
             let label = {type:"label", id:labeluuid, value:"", parent:parent}
             resultArray.push(label)
@@ -42,6 +66,7 @@ function ContentArea (props) {
             resultingObj = {type:type, parent:parent, id:myuuid, value:"", label:""}
         }
         resultArray.push(resultingObj)
+        console.log(resultArray)
         let firstHalf = current.slice(0, position + 1)
         let secondHalf = current.slice(position + 1)
         let result = firstHalf.concat(resultArray, secondHalf)
@@ -54,24 +79,24 @@ function ContentArea (props) {
 
         let topLevel = current.filter(el => {return el.parent === topParent})
         
-        result.push(<LineBetween insert={insertElement} myParent={topParent} first></LineBetween>)
+        result.push(<LineBetween insert={insertElement} key={topParent + "line at beginning"} myBefore={topParent} first></LineBetween>)
 
         for (const el of topLevel) {
             let label = topLevel.find((e) => e.type === "label" && e.id === el.label)
 
             if (el.type === "object") {
-                result.push(<ObjectDisplay key={el.id} label={label}>{displayElements(el.id)}</ObjectDisplay>)
+                result.push(<ObjectDisplay key={el.id} id={el.id} label={label} change={changeElement}>{displayElements(el.id)}</ObjectDisplay>)
                 if (topParent !== "1") {
-                    result.push(<LineBetween insert={insertElement} myBefore={el.id}></LineBetween>)
+                    result.push(<LineBetween insert={insertElement} myBefore={el.id} key={el.id + "line after"}></LineBetween>)
                 }
             }
             if (el.type === "array") {
-                result.push(<ArrayDisplay key={el.id} label={label}>{displayElements(el.id)}</ArrayDisplay>)
-                result.push(<LineBetween insert={insertElement} myBefore={el.id}></LineBetween>)
+                result.push(<ArrayDisplay key={el.id} id={el.id} label={label} change={changeElement}>{displayElements(el.id)}</ArrayDisplay>)
+                result.push(<LineBetween insert={insertElement} myBefore={el.id} key={el.id + "line after"}></LineBetween>)
             }
             if (el.type === "number" || el.type === "string" || el.type === "boolean") {
-                result.push(<ValueDisplay key={el.id} value={el.value} label={label}></ValueDisplay>)
-                result.push(<LineBetween insert={insertElement} myBefore={el.id}></LineBetween>)
+                result.push(<ValueDisplay key={el.id} id={el.id} value={el.value} label={label} change={changeElement}></ValueDisplay>)
+                result.push(<LineBetween insert={insertElement} myBefore={el.id} key={el.id + "line after"}></LineBetween>)
             }
         }
 
@@ -88,30 +113,46 @@ function ContentArea (props) {
 function ObjectDisplay (props) {
 
     return (<div className="Object" style={{display:"flex", flexDirection:"row", height:"content", width:"content", padding:".2rem 1rem", margin:".2rem 1rem"}}>
-        {props.label && <LabelDisplay key={props.label.id} value={props.label.value}></LabelDisplay>}
+        {props.label && <LabelDisplay change={props.change} key={props.label.id} id={props.label.id} value={props.label.value}></LabelDisplay>}
         <div className="ObjectContents" style={{display:"flex", flexDirection:"column", backgroundColor:"pink", height:"content", width:"content", padding:".2rem 1rem", margin:".2rem 1rem", border:"4px solid grey"}}>{props.children}</div>
         </div>)
 }
 
 function LabelDisplay (props) {
 
+    const [editing, setEditing] = useState(false)
+    const [myValue, setMyValue] = useState(props.value)
 
-    return (<div className="Label" style={{backgroundColor:"salmon", height:"content", width:"content", padding:".2rem 1rem", margin:".2rem 1rem"}}><p>{props.value}</p></div>)
+    if (editing) {
+        return (<div className="Label" style={{backgroundColor:"salmon", height:"content", width:"content", padding:".2rem 1rem", margin:".2rem 1rem"}} onBlur={() => {setEditing(false); props.change(props.id, myValue)}} ><input type="text" defaultValue={myValue} onChange={(e) => setMyValue(e.target.value)}></input></div>)
+    }
+    else {
+        return (<div className="Label" style={{backgroundColor:"salmon", height:"content", width:"content", padding:".2rem 1rem", margin:".2rem 1rem"}} onClick={() => setEditing(true)}><p>{myValue}</p></div>)
+    }
+
 }
 
 function ValueDisplay (props) {
+    const [editing, setEditing] = useState(false)
+    const [myValue, setMyValue] = useState(props.value)
 
-
-    return (<div className="Value" style={{display:"flex", alignItems:"center", backgroundColor:"lightpink", height:"content", width:"content", padding:".2rem 1rem", margin:".2rem 1rem"}}>
-        {props.label && <LabelDisplay key={props.label.id} value={props.label.value}></LabelDisplay>}
-        <p defaultValue={props.value}>{props.value}</p></div>)
+    if (editing) {
+        return (<div className="Value" style={{display:"flex", alignItems:"center", backgroundColor:"lightpink", height:"content", width:"content", padding:".2rem 1rem", margin:".2rem 1rem"}} onBlur={() => {setEditing(false); props.change(props.id, myValue)}} >
+            {props.label && <LabelDisplay key={props.label.id} id={props.label.id} value={props.label.value} change={props.change}></LabelDisplay>}
+            <input type="text" defaultValue={myValue} onChange={(e) => setMyValue(e.target.value)}></input><p style={{color:"blue"}}>{props.type}</p></div>)
+    }
+    else {
+        return (<div className="Value" style={{display:"flex", alignItems:"center", backgroundColor:"lightpink", height:"content", width:"content", padding:".2rem 1rem", margin:".2rem 1rem"}} onClick={() => setEditing(true)} >
+            {props.label && <LabelDisplay key={props.label.id} id={props.label.id} value={props.label.value} change={props.change}></LabelDisplay>}
+            <p>{myValue}</p><p style={{color:"blue"}}>{props.type}</p></div>)
+    }
 }
 
 function ArrayDisplay (props) {
 
 
     return (<div className="Array" style={{display:"flex", flexDirection:"row", flexWrap:"wrap", height:"content", width:"content", padding:".2rem 1rem", margin:".2rem 1rem"}}>
-        {props.label && <LabelDisplay key={props.label.id} value={props.label.value}></LabelDisplay>}
+        {props.label && <LabelDisplay key={props.label.id} id={props.label.id} value={props.label.value} change={props.change}></LabelDisplay>}
         <div className="ArrayContents" style={{display:"flex", flexDirection:"row", backgroundColor:"pink", height:"content", width:"content", padding:".2rem 1rem", margin:".2rem 1rem", border:"4px solid brown"}}>{props.children}</div></div>)
 }
 
